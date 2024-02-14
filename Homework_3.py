7e372f092002a413f0879be526c3c9ddec1533ee
Mangoost <mangoost81@gmail.com>
2024-02-14 14:18:46 +0300

    ready to push

--- Homework_3.py
+++ Homework_3.py
@@ -53,21 +53,51 @@ word = "DErrim"
 
 
 
-count = 0
-for letter in word:
-    for key in russian:
-        if letter.upper() in key:
-            count += russian.get(key)
+# count = 0
+# for letter in word:
+#     for key in russian:
+#         if letter.upper() in key:
+#             count += russian.get(key)
 
-if count > 0:
-    print(count)
+# if count > 0:
+#     print(count)
 
-count = 0
-for letter in word:
-    for key in english:
-        if letter.upper() in key:
-            count += english.get(key)
+# count = 0
+# for letter in word:
+#     for key in english:
+#         if letter.upper() in key:
+#             count += english.get(key)
 
-if count > 0:
-    print(count)
+# if count > 0:
+#     print(count)
 
+
+# count = 0
+# for letter in word.upper():
+#     for (key,item) in english.items():
+#         if letter in key:
+#             count += item
+
+# print(count)
+
+
+def task1(input_str):
+    char_count = {}
+    result = []
+
+characters = input_str.split()
+
+for char in characters:
+    if char in char_count:
+# char_count[char] += 1
+        print(f"{char}_{char_count[char]}", end=' ')
+    else:
+# char_count[char] = 0 # a = 0 -> a a = 1
+        print(char, end=' ')
+#result.append(char)
+char_count[char] = char_count.get(char, 0) + 1
+#return ' '.join(result)
+
+
+input_str = "a a a b c a a d c d d" # a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+task1(input_str)
